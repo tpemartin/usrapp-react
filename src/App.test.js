f97b956edda4626b4c18abb707ff1ae6ff7e8f5b
tpemartin <tpemartin@gmail.com>
2022-09-11 22:33:53 +0800

    meta

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import createStoryPageContent from "./modules/page_story.js";
-import fetchEventJson, {carousel, eventPageContent, clickGetElement} from "./modules/page_event.js";
+import fetchEventJson, 
+  {carousel, eventPageContent, clickGetElement,
+    eventModal, eventTime} from "./modules/page_event.js";
 import { promiseCheck} from "./modules/Debug.js";
 
 $(function(){
@@ -7,10 +9,26 @@ $(function(){
     $("#storyPage").click(fillInStoryPage)
     // sidebar eventPage click 
     $("#eventPage").click(fillInEventPage)
+
+    let id=getQueryParameter()
+    if(id) eventPageForShare(id)
    })
 
-function fillInEventPage(){
-    fetchEventJson()
+async function eventPageForShare(id){
+  fillInEventPage()
+    .then((eventInfo)=>{
+      console.log(eventInfo)
+      //console.log(window.eventMap[id])
+      let index=eventInfo.eventMap[id]
+      $("#carouselExampleCaptions").carousel(index).carousel("pause")//window.eventMap[id])
+      let ev=eventInfo.events[index]
+      let metaTag= meta("https://tpemartin.github.io/usrapp-react/src/index.html?id="+ev.id, ev.活動名稱, ev.活動內容簡述, ev['海報網址'])
+      console.log(metaTag) 
+      $("head").append(metaTag)
+    })
+}
+async function fillInEventPage(){
+    return fetchEventJson()
       .then((eventJson)=>{
         let events = eventJson
             .slice(1, eventJson.length+1) //remove title row
@@ -23,8 +41,12 @@ function fillInEventPage(){
         window.events=eventCarousel.events
         window.eventMap=eventCarousel.eventMap
         clickGetElement(getClickEvent)
+        
         $("body").prepend(`<div id="modal" class="row justify-content-center"></div>`)
+
+        return {events: eventCarousel.events, eventMap: eventCarousel.eventMap}
       })
+ 
 }
 
 function fillInStoryPage(){
@@ -43,8 +65,33 @@ function fillInStoryPage(){
     })
 
 }
- 
+
+function createMeta(){
+  let id=getQueryParameter()
+  if(id){
+    fillInEventPage()
+      .then()
+  }
+}
+
 //helpers
+function meta(url, title, description="", imgSrc=""){
+
+return `<meta property="og:url"           content="${url}" />
+<meta property="og:type"          content="web app" />
+<meta property="og:title"         content="${title}" />
+<meta property="og:description"   content="${description}" />
+<meta property="og:image"         content="${imgSrc}/>`
+}
+
+function getQueryParameter(){
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  return params.id; // "some_value"
+  
+}
 
 function getClickEvent(e){
     console.log(e)
@@ -53,7 +100,8 @@ function getClickEvent(e){
     let targetEvent=events[eventIndex]
     console.log(targetEvent)
     let evModal=eventModal(targetEvent['活動名稱'], eventTime(targetEvent), 
-    targetEvent['活動內容簡述'], targetEvent['地點'], targetEvent['主辦單位細節']
+    targetEvent['活動內容簡述'], targetEvent['地點'], targetEvent['主辦單位細節'],
+    targetEvent["Google行事曆事件訂閱連結"],targetEvent['活動報名網址（若無免填）']
     )
     console.log(evModal)
     $("#modal").empty()
@@ -67,143 +115,6 @@ function getClickEvent(e){
         `
       )
     $("#exampleModal").modal("show")  
+    $("#shareEventBtn").click(()=>{$("#shareBox").toggle()})
     return targetEvent
 }
-
-
-//helpers
-function eventModal(title="北大玩具節", time="2018.11.10", content="現場凡攜帶項二手玩具或2張111年1-4月發票. 報名方式: 至服務台捐出,即可領取闖關卡入場券。(絨毛娃娃不在回收範圍裡喔!)",
-_location="...", organizer="..."){
-
-
-  return {
-    "button": `
-  <!-- Button trigger modal -->
-  <button id="eventModal" style="display:none" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-    詳細內容
-  </button>`,
-    "modal": 
-  `<!-- Modal -->
-  <div class="modal fade text-center" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog  modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h3 class="modal-title">${title}</h3>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-header">
-          <h4 class="modal-title">${time}</h4>
-        </div>
-        <div class="modal-body">
-          ${tabNav()}
-        <br>
-          ${tabContent(content, _location, organizer)}
-        </div>
-        <div class="modal-footer justify-content-center">
-          ${eventDecision()}
-        </div>
-      </div>
-    </div>
-  </div>
-  `}
-}
-function eventModalButton(){
-  return `
-  <!-- Button trigger modal -->
-  <button id="eventDetailBtn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-    詳細內容
-  </button>`
-}
-
-
-function eventTime(ev){
-  let formatCase=getFormatCase(ev)
-  switch(formatCase.formatCase){
-    case "a":
-       return formatCase.activityTime.A
-    case "ab":
-      return `${formatCase.activityTime.A} ${formatCase.activityTime.B}`
-
-    case "ay":
-      return `${formatCase.activityTime.A} 到 ${formatCase.activityTime.Y}`
-
-    case "abz":
-      return `${formatCase.activityTime.A} ${formatCase.activityTime.B}-${formatCase.activityTime.Z}`
-
-    case "abyz":
-      return `${formatCase.activityTime.A} 到 ${formatCase.activityTime.Y}
-      ${ev["頻率"]} ${formatCase.activityTime.B}-${formatCase.activityTime.Z}`
-}
-
-
-}
-function ymd(timeString){
-  let startD=new Date(timeString)
-  return startD.getFullYear()+'.'+startD.getMonth()+"."+startD.getDay()
-}
-function hm(timeString){
-  let startD=new Date(timeString)
-  let hmString=startD.toLocaleTimeString().replace(/:[0-9][0-9] (?=[PA])/,"")
-  return hmString
-}
-function getFormatCase(ev){
-  var formatCaseMap= {"日期(1)": "a","時間(1)": "b","日期(2)": "y","時間(2)": "z",}
-
-  var formatCase=""
-  let activityTime={}
-  if(ev["日期(1)"]!==""){
-    activityTime.A=ymd(ev['日期(1)']) 
-    formatCase=formatCase+formatCaseMap["日期(1)"]}
-  if(ev["時間(1)"]!==""){ 
-    activityTime.B=hm(ev['時間(1)']) 
-    formatCase=formatCase+formatCaseMap["時間(1)"]}
-  if(ev["日期(2)"]!==""){ 
-    activityTime.Y=ymd(ev['日期(2)']) 
-    formatCase=formatCase+formatCaseMap["日期(2)"]}
-  if(ev["時間(2)"]!==""){ 
-    activityTime.Z=hm(ev['時間(1)']) 
-    formatCase=formatCase+formatCaseMap["時間(2)"]}
-
-  return { formatCase: formatCase, activityTime: activityTime }
-}
-
-function eventDecision(){
-  return `
-  <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
-  
-    <div class="btn-group mr-2" role="group" aria-label="Second group">
-      <button type="button" class="btn btn-secondary">分享</button>
-    </div>
-    <div class="btn-group" role="group" aria-label="Third group">
-      <button type="button" class="btn btn-secondary">加入行事曆</button>
-    </div>
-  </div>`
-}
-
-
-function tabNav(){
-  return ` 
-  
-  <nav>
-  <div class="nav nav-pills  justify-content-center" id="nav-tab" role="tablist">
-    <button type="button" class="btn btsm btn-outline-primary nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">內容</button>
-    <button type="button" class="btn btsm btn-outline-primary nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">地點</button>
-    <button type="button" class="btn btsm btn-outline-primary nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">主辦單位</button>
-  </div>
-</nav>
-
-`
-}
-
-function tabContent(content, location, organizer){
-  return `
-  <div class="tab-content" id="nav-tabContent" style="height:60vh;">
-  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">${content}</div>
-  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">${location}</div>
-  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">${organizer}</div>
-</div>
-  `
-}
-
--- src/modules/page_event.js
+++ src/modules/page_event.js
@@ -4,7 +4,6 @@ export function eventPageContent(eventCarousel){
         ${eventCarousel.carouselhtml}
         </div>
     <div>
-    ${footer(eventButtons())}
     `
 }
 export default async function fetchEventJson(){
@@ -22,12 +21,13 @@ function footer(element){
     </div>`
 }
 
+
 function eventButtons(){
     return `<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
     <div class="btn-group mr-2" role="group" aria-label="First group">
       <button type="button" class="btn btn-secondary">詳細內容</button>
     </div>
-    <div class="share-group">
+    <div class="share-group" style="height:0;">
     <div class="share-group" id="share-icon" style="display:none">
       ${share()}
       ${share()}
@@ -37,7 +37,8 @@ function eventButtons(){
     </div>
     </div>
     <div class="btn-group" role="group" aria-label="Third group">
-      <button type="button" class="btn btn-secondary">加入行事曆</button>
+      
+      <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=NDM3bW50cnZpYW50Ymk2N2N2ZXN0ZTJqNDcgZ20ubnRwdS5lZHUudHdfY2xhc3Nyb29tMDkwNTg4ODlAZw&amp;tmsrc=gm.ntpu.edu.tw_classroom09058889%40group.calendar.google.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"><button type="button" class="btn btn-secondary">加入行事曆</button></a>
     </div>
     
   </div>`
@@ -115,3 +116,171 @@ function constructEventMap(events){
   })
   return eventMap 
 }
+
+//Event Modal
+//helpers
+function eventModal(title="北大玩具節", time="2018.11.10", content="現場凡攜帶項二手玩具或2張111年1-4月發票. 報名方式: 至服務台捐出,即可領取闖關卡入場券。(絨毛娃娃不在回收範圍裡喔!)",
+_location="...", organizer="...", calendarUrl="",registerUrl=""){
+  let calendarHtml=""
+  if(calendarUrl !== ""){
+    calendarHtml=googleCalendar(calendarUrl)
+  } 
+  let registerHtml=""
+  if(registerUrl !== ""){
+    registerHtml=registerBtn(registerUrl)
+  } 
+  return {
+    "button": `
+  <!-- Button trigger modal -->
+  <button id="eventModal" style="display:none" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    詳細內容
+  </button>`,
+    "modal": 
+  `<!-- Modal -->
+  <div class="modal fade text-center" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">${title}</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-header">
+          <h4 class="modal-title">${time}</h4>
+        </div>
+        <div class="modal-body">
+          ${tabNav()}
+        <br>
+          ${tabContent(content, _location, organizer)}
+        </div>
+        <div class="modal-footer justify-content-center">
+          ${eventDecision(calendarHtml, registerHtml)}
+        </div>
+      </div>
+    </div>
+  </div>
+  `}
+}
+function eventModalButton(){
+  return `
+  <!-- Button trigger modal -->
+  <button id="eventDetailBtn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    詳細內容
+  </button>`
+}
+
+
+function eventTime(ev){
+  let formatCase=getFormatCase(ev)
+  switch(formatCase.formatCase){
+    case "a":
+       return formatCase.activityTime.A
+    case "ab":
+      return `${formatCase.activityTime.A} ${formatCase.activityTime.B}`
+
+    case "ay":
+      return `${formatCase.activityTime.A} 到 ${formatCase.activityTime.Y}`
+
+    case "abz":
+      return `${formatCase.activityTime.A} ${formatCase.activityTime.B}-${formatCase.activityTime.Z}`
+
+    case "abyz":
+      return `${formatCase.activityTime.A} 到 ${formatCase.activityTime.Y}
+      ${ev["頻率"]} ${formatCase.activityTime.B}-${formatCase.activityTime.Z}`
+}
+
+
+}
+function ymd(timeString){
+  let startD=new Date(timeString)
+  return startD.getFullYear()+'.'+startD.getMonth()+"."+startD.getDay()
+}
+function hm(timeString){
+  let startD=new Date(timeString)
+  let hmString=startD.toLocaleTimeString().replace(/:[0-9][0-9] (?=[PA])/,"")
+  return hmString
+}
+function getFormatCase(ev){
+  var formatCaseMap= {"日期(1)": "a","時間(1)": "b","日期(2)": "y","時間(2)": "z",}
+
+  var formatCase=""
+  let activityTime={}
+  if(ev["日期(1)"]!==""){
+    activityTime.A=ymd(ev['日期(1)']) 
+    formatCase=formatCase+formatCaseMap["日期(1)"]}
+  if(ev["時間(1)"]!==""){ 
+    activityTime.B=hm(ev['時間(1)']) 
+    formatCase=formatCase+formatCaseMap["時間(1)"]}
+  if(ev["日期(2)"]!==""){ 
+    activityTime.Y=ymd(ev['日期(2)']) 
+    formatCase=formatCase+formatCaseMap["日期(2)"]}
+  if(ev["時間(2)"]!==""){ 
+    activityTime.Z=hm(ev['時間(1)']) 
+    formatCase=formatCase+formatCaseMap["時間(2)"]}
+
+  return { formatCase: formatCase, activityTime: activityTime }
+}
+function facebookShare(){
+  return `<div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>`
+}
+function eventDecision(calendarHtml,registerHtml){
+  return `
+  <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="share-group" style="height:0">
+    <div class="share-group" id="shareBox" style="display:none">
+      <div class="btn-group mr-2" role="group" aria-label="Second group">  
+    <button type="button" class="btn btn-secondary">分享2</button>
+    </div>
+      <div class="btn-group mr-2" role="group" aria-label="Second group">  
+   ${facebookShare()}
+    </div>
+    </div>
+    <div class="btn-group mr-2" role="group" aria-label="Second group">
+   
+      <button type="button" class="btn btn-secondary" id="shareEventBtn">分享</button>
+    </div>
+    </div>
+    ${registerHtml}
+    ${calendarHtml}
+  </div>`
+}
+function googleCalendar(calendarUrl){
+  return `<div class="btn-group" role="group" aria-label="Third group">
+  <a target="_blank" href="${calendarUrl}">
+    <button type="button" class="btn btn-secondary">加入行事曆</button>
+  </a> 
+  </div>`
+}
+function registerBtn(registerUrl){
+  return `<div class="btn-group" role="group" aria-label="Fourth group">
+  <a target="_blank" href="${registerUrl}">
+    <button type="button" class="btn btn-secondary">報名</button>
+  </a> 
+  </div>`
+}
+function tabNav(){
+  return ` 
+  
+  <nav>
+  <div class="nav nav-pills  justify-content-center" id="nav-tab" role="tablist">
+    <button type="button" class="btn btsm btn-outline-primary nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">內容</button>
+    <button type="button" class="btn btsm btn-outline-primary nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">地點</button>
+    <button type="button" class="btn btsm btn-outline-primary nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">主辦單位</button>
+  </div>
+</nav>
+
+`
+}
+
+function tabContent(content, location, organizer){
+  return `
+  <div class="tab-content" id="nav-tabContent" style="height:60vh;">
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">${content}</div>
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">${location}</div>
+  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">${organizer}</div>
+</div>
+  `
+}
+
+export {eventModal, eventTime}
